bf7d5b1f1b3de202657effd71e9d2b6fb8c85295
Marksiiuu <marcofloyd69@gmail.com>
2026-04-28 20:04:07 +0800

    update docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,16 +1,53 @@
 <?php
 
+
+
 namespace App\Providers;
 
+
+
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
+
+use Illuminate\Support\Facades\URL;
+
+
 
 class AppServiceProvider extends ServiceProvider
+
 {
-    public function register(): void {}
+
+    /**
+
+     * Register any application services.
+
+     */
+
+    public function register(): void
+
+    {
+
+        //
+
+    }
+
+
+
+    /**
+
+     * Bootstrap any application services.
+
+     */
 
     public function boot(): void
+
     {
-        Paginator::useBootstrapFive();
+
+        if (env('APP_ENV') === 'production') {
+
+            URL::forceScheme('https');
+
+        }
+
     }
+
 }
